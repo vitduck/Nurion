b69e9b7a6be40ad631f67ae55e84b7af751f99dd
vitduck <levietduc@kaist.ac.kr>
2019-03-25 13:12:46 +0900

    Add build script for ELPA

---
+++ elpa/build_intel_legacy.sh
@@ -0,0 +1,20 @@
+# build environement
+# adapt form xconfigure
+# this version of elpa does not contain avx512 kernel
+
+export FC=mpiifort
+export FCFLAGS="-Ofast $(CRAY_xAVX) -align array64byte -heap-arrays 4096"
+
+export CC=mpiicc 
+export CFLAGS="-Ofast $(CRAY_xAVX) -fno-alias -ansi-alias"
+
+export PREFIX="$HOME/build"
+export ELPA_VER="2016.11.001.pre"
+
+../configure \
+    --with-avx-optimization \
+    --prefix=$PREFIX \
+    --libdir=$PREFIX/lib/$COMPILER/$COMPILER_VER/$CRAY_CPU_TARGET/elpa/$ELPA_VER \
+    --includedir=$PREFIX/include/$COMPILER/$COMPILER_VER/$CRAY_CPU_TARGET/elpa \
+    SCALAPACK_LDFLAGS="-L${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_blacs_intelmpi_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential" \
+    SCALAPACK_FCFLAGS="-L${MKLROOT}/include" 
